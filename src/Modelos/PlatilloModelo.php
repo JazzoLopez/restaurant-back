7b0567fec0a010adc47da5128c47d69f25d8da23
JazzoLopez <jazzielrodriguezlopez@gmail.com>
2024-05-10 19:05:29 -0600

    Add: agregado funcion

--- src/Modelos/PlatilloModelo.php
+++ src/Modelos/PlatilloModelo.php
@@ -1,19 +1,40 @@
 <?php
 namespace App\Modelos;
-use App\Modelos\DbModel,
-    App\Lib\Response;
 
-class PlatilloModelo{
-    private $response;  
+use App\Modelos\DbModel,
+App\Lib\Response;
+
+class PlatilloModelo
+{
+    private $db = null;
+    private $response;
     private $nombre = 'name';
     private $descripcion = 'description';
     private $precio = 'price';
+    private $image = 'image';
+    private $tbSaucers = 'saucers';
 
-    public function __construct(){
+    public function __construct()
+    {
         $db = new DbModel();
-        $this -> db = $db->sqlPDO;
-        $this -> response = new Response();
+        $this->db = $db->sqlPDO;
+        $this->response = new Response();
+    }
+
+    public function nuevoPlatillo($body)
+    {
+        $data = [
+            'name' => $body->name,
+            'description' => $body->description,
+            'price' => $body->price,
+            'image' => $body->image
+        ];
+        $result = $this -> db ->insertInto($this -> tbSaucers) -> values($data);
+        if($result){
+        $this -> response -> result = $result;
+        return $this->response->SetResponse(true, 'Platillo registrado');
+        } else {
+            return $this->response->SetResponse(false,'Error al registrar ');
         }
-    
-    
+    }
 }
--- src/Modelos/ReservacionModelo.php
+++ src/Modelos/ReservacionModelo.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Modelos;
-use App\Modelos\DbModel, 
-    App\Lib\Response;
+
+use App\Modelos\DbModel,
+App\Lib\Response;
 
 
-class ReservacionModelos{
+class ReservacionModelos
+{
     private $response;
     private $tbReservaciones = 'reservations';
     private $db = null;
@@ -13,50 +15,53 @@ class ReservacionModelos{
     private $comments = 'comments';
     private $userID = 'user_id';
 
-    public function __construct(){
+    public function __construct()
+    {
         $db = new DbModel();
-        $this -> db = $db->sqlPDO;
-        $this -> response = new Response();
-        }
+        $this->db = $db->sqlPDO;
+        $this->response = new Response();
+    }
 
-        public function verReservaciones($body){ 
-            $id = $body -> id;
-            $resutl = $this->db->from( $this-> tbReservaciones)->where($this -> userID, $id )->fetchAll();
-            if(count($resutl) > 0){
-                $this -> response -> result = $resutl;
-            return $this->response->SetResponse(true,"Datos pintados correctamente");
-            } else {
-                return $this->response->SetResponse(false,"El Id de la reservación no existe");
-            }
+    public function verReservaciones($body)
+    {
+        $id = $body->id;
+        $resutl = $this->db->from($this->tbReservaciones)->where($this->userID, $id)->fetchAll();
+        if (count($resutl) > 0) {
+            $this->response->result = $resutl;
+            return $this->response->SetResponse(true, "Datos pintados correctamente");
+        } else {
+            return $this->response->SetResponse(false, "El Id de la reservación no existe");
         }
+    }
 
-        public function nuevaReservacion($body){
-            $validate = $this -> db -> from ($this -> tbReservaciones)
-                -> where($this -> hour, $body -> hour) -> where($this ->date, $body -> date) ->count();
-             if($validate>0){
-                return $this -> response -> SetResponse(false,"La hora de reservación ya esta ocupada");
-             }
-             $data = [
-                'date' => $body -> date,
-                'hour'=> $body -> hour,
-                'comments' => $body -> comments,
-                'user_id'=> $body -> user_id
-             ];
-            $result = $this -> db -> insertInto($this -> tbReservaciones)->values( $data )->execute();
-            if(!$result){
-                return $this -> response -> SetResponse(false,'');
-            }
-            //$this -> response -> result = $result;
-            return $this -> response ->SetResponse(true,'Reservación guardada correctamente');
+    public function nuevaReservacion($body)
+    {
+        $validate = $this->db->from($this->tbReservaciones)
+            ->where($this->hour, $body->hour)->where($this->date, $body->date)->count();
+        if ($validate > 0) {
+            return $this->response->SetResponse(false, "La hora de reservación ya esta ocupada");
         }
+        $data = [
+            'date' => $body->date,
+            'hour' => $body->hour,
+            'comments' => $body->comments,
+            'user_id' => $body->user_id
+        ];
+        $result = $this->db->insertInto($this->tbReservaciones)->values($data)->execute();
+        if (!$result) {
+            return $this->response->SetResponse(false, '');
+        }
+        //$this -> response -> result = $result;
+        return $this->response->SetResponse(true, 'Reservación guardada correctamente');
+    }
 
-        public function eliminarReservacion($id){
-            $validate = $this -> db -> deleteFrom($this -> tbReservaciones)->where('id',$id)->execute();
-            if($validate> 0){
-                return $this -> response -> SetResponse(true,'Reservacion eliminada correctamente');
-            }
-            else {
-                return $this -> response -> SetResponse(false,'Error al eliminar');
-            }
+    public function eliminarReservacion($id)
+    {
+        $validate = $this->db->deleteFrom($this->tbReservaciones)->where('id', $id)->execute();
+        if ($validate > 0) {
+            return $this->response->SetResponse(true, 'Reservacion eliminada correctamente');
+        } else {
+            return $this->response->SetResponse(false, 'Error al eliminar');
         }
+    }
 }
