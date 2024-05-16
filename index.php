425ae9d144216d825422096e6f37239362c5563c
JazzoLopez <jazzielrodriguezlopez@gmail.com>
2024-05-15 23:37:47 -0600

    Fix: regresando headers

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: *");
 header('Content-Type: application/json');
     
 use Psr\Http\Message\ResponseInterface as Response;
